f42da37fc0785df771ba560052c55f8f18c63414
sunggeun <qwer123@naver.com>
2022-06-10 18:51:51 +0900

    main

--- src/team/TeamMain.java
+++ src/team/TeamMain.java
@@ -1,65 +1,28 @@
 package team;
 
-import java.util.ArrayList;
 import java.util.Scanner;
-class StInfo{
-	private String name;
-	private int id;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-}
+
 public class TeamMain {
+	
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		ArrayList<StInfo> st = new ArrayList<StInfo>();
-		System.out.println("학생관리프로그램");
 		
-		boolean run = true;
+		Scanner scan = new Scanner(System.in);
+		int select;
+		System.out.println("1.최나윤이동");
+		System.out.println("2.임하형이동");
+		System.out.println("3.손성근이동");
+		System.out.println("4.종료");
+		System.out.print(">>>> ");
+		select = scan.nextInt();
 		
-		
-		while(run) {
-			System.out.println("1.학생 등록");
-			System.out.println("2.학생 검색");
-			System.out.println("3.학생 삭제");
-			System.out.println("4.수정");
-			System.out.println("5.모든학생 출력");
-			System.out.println("6.종료");
-			int num = input.nextInt();
+		if(select == 1) {
 			
-			switch(num) {
-			case 1:
-				
-				break;
-			case 2:
-				
-				break;
-			case 3:
-				
-				break;
-			case 4:
-				
-				break;
-			case 5:
-				
-				break;
-			case 6:
-				
-				break;
+		}else if(select == 2) {
 			
-			}
+		}else if(select == 3) {
+			
+		}else {
+			System.exit(0);
 		}
 	}
-	
-	
-	
 }	
